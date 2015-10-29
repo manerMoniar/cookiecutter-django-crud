09a36190a2461e9f3c76511f84d2abcce4a9df14
Maner Moniar <maner.moniar@gmail.com>
2015-10-28 18:45:51 -0600

    Static folders & messages

--- {{cookiecutter.app_name}}/views.py
+++ {{cookiecutter.app_name}}/views.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import reverse_lazy
 from vanilla import CreateView, DetailView, UpdateView, DeleteView
 from django_tables2 import SingleTableView
+from django.contrib.messages.views import SuccessMessageMixin
 from .forms import {{ cookiecutter.model_name }}Form
 from .models import {{ cookiecutter.model_name }}
+from .tables import {{ cookiecutter.model_name }}Table
 
 
 class {{ cookiecutter.model_name }}List(SingleTableView):
@@ -10,20 +12,22 @@ class {{ cookiecutter.model_name }}List(SingleTableView):
     table_class = {{ cookiecutter.model_name }}Table
 
 
-class {{ cookiecutter.model_name }}Create(CreateView):
+class {{ cookiecutter.model_name }}Create(SuccessMessageMixin, CreateView):
     model = {{ cookiecutter.model_name }}
     form_class = {{ cookiecutter.model_name }}Form
     success_url = reverse_lazy('{{ cookiecutter.app_name }}:list')
+    success_message = "%(name)s was created successfully"
 
 
 class {{ cookiecutter.model_name }}Detail(DetailView):
     model = {{ cookiecutter.model_name }}
 
 
-class {{ cookiecutter.model_name }}Update(UpdateView):
+class {{ cookiecutter.model_name }}Update(SuccessMessageMixin, UpdateView):
     model = {{ cookiecutter.model_name }}
     form_class = {{ cookiecutter.model_name }}Form
     success_url = reverse_lazy('{{ cookiecutter.app_name }}:list')
+    success_message = "%(name)s was updated successfully"
 
 
 class {{ cookiecutter.model_name }}Delete(DeleteView):
