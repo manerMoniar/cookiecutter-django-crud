e02ef245edd0c73a5c6dd27a2f5e29b602858689
Maner Moniar <maner.moniar@gmail.com>
2015-12-14 15:06:44 -0600

    Autofocus first field

--- {{cookiecutter.app_name}}/forms.py
+++ {{cookiecutter.app_name}}/forms.py
@@ -4,6 +4,8 @@ from .models import {{ cookiecutter.model_name }}
 
 class {{ cookiecutter.model_name }}Form(forms.ModelForm):
 	
+	name = forms.CharField(widget=forms.TextInput(attrs={'autofocus':''}))
+
     class Meta:
         model = {{ cookiecutter.model_name }}
         fields = ['name', ]
